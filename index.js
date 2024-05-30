c7b565de36f67a9dca0abb67c4b15a153dd14f67
Aliyv019 <tahiraliyev2006@gmail.com>
2024-05-30 17:47:40 +0400

    updated

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
-//task 1 duzbucaqlinin perimetri ve sahesini hesabla
-// for(var i=true;i==true;){
-//     var width=prompt("enter the width of rectangle")
-//     var height=prompt("enter the height of rectangle")
-//     alert(`perimeter of rectangle:${width*2+height*2}, area of rectangle:${width*height}`);
-// }
-
-//task 1.1? ucbucagin movcud olub olmamasi ve movcuddusa ucuncu bucagin tapilmasi
+//task 1 ucbucagin movcud olub olmamasi ve movcuddusa ucuncu bucagin tapilmasi
 // for(var i=true;i==true;){
 //     var a=+prompt("please enter first corner")
 //     var b=+prompt("please enter second corner")
@@ -18,7 +11,7 @@
 //     }
 // }
 
-//task 2
+// task 2
 // var arr=[45,67,89,23,55,88,93]
 // var f=false
 // var a=+prompt("enter a random number")
@@ -35,17 +28,21 @@
 // }
 
 ///task 3 ededlerin cemini,ededi ortasini, sayini, 100-den boyuk 200 den kicik ededlerin sayini, cut ededlerin sayini tap
-// var arr=[678,45,67,89,23,545,188,293]
-// var sum=0
-// var b=0
-// var even=0
-// for(var i=0;i<arr.length;i++){
-//     sum+=arr[i]
-//     if(arr[i]>100 && arr[i]<200){
-//         b++
-//     }
-//     if(arr[i]%2==0){
-//         even++
-//     }
-// }
-// alert(`sum of numbers from our array:${sum}\naverage of numbers from our array:${sum/arr.length}\nthe number of numbers in our array:${arr.length}\nThe number of numbers greater than 100 and less than 200 in the array:${b}\nThe number of even numbers in the array:${even}`)
+var arr=[678,45,67,89,23,545,188,293]
+var sum=0
+var b=0
+var even=0
+var odd=0
+for(var i=0;i<arr.length;i++){
+    sum+=arr[i]
+    if(arr[i]>100 && arr[i]<200){
+        b++
+    }
+    if(arr[i]%2==0){
+        even++
+    }
+    else{
+        odd++
+    }
+}
+alert(`sum of numbers from our array:${sum}\naverage of numbers from our array:${sum/arr.length}\nthe number of numbers in our array:${arr.length}\nThe number of numbers greater than 100 and less than 200 in the array:${b}\nThe number of even numbers in the array:${even}\nThe number of odd numbers in the array:${odd}`)
